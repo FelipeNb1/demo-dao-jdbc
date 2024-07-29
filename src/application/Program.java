57d1f8b24ce27ff0a273be9c8a8f299d3f348917
FelipeNb1 <nunessfelipee45@gmail.com>
2024-07-29 09:48:15 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,5 +26,11 @@ public class Program {
         for (Seller s : list) {
             System.out.println(s);
         }
+
+        System.out.println("\n=== TEST 3: Seller  findByDepartament===");
+        list = sellerDao.findAll();
+        for (Seller s : list) {
+            System.out.println(s);
+        }
     }
 }
